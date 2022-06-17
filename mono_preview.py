cf831f0dcad926981bc169c96e1f4360b2fb038d
Dinchen Tech <frank@dinchen.com.tw>
2022-06-17 11:30:40 +0800

    about 100fps

---
+++ mono_preview.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import cv2
+import depthai as dai
+import time
+
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+xoutLeft = pipeline.create(dai.node.XLinkOut)
+xoutRight = pipeline.create(dai.node.XLinkOut)
+
+xoutLeft.setStreamName('left')
+xoutRight.setStreamName('right')
+
+# Properties
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setFps(120)
+monoRight.setFps(120)
+
+# Linking
+monoRight.out.link(xoutRight.input)
+monoLeft.out.link(xoutLeft.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    # Output queues will be used to get the grayscale frames from the outputs defined above
+    qLeft = device.getOutputQueue(name="left", maxSize=4, blocking=False)
+    qRight = device.getOutputQueue(name="right", maxSize=4, blocking=False)
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
+
+    while True:
+        # Instead of get (blocking), we use tryGet (non-blocking) which will return the available data or None otherwise
+        inLeft = qLeft.get()
+        inRight = qRight.get()
+
+
+        cv2.imshow("left", inLeft.getCvFrame())
+        cv2.imshow("right", inRight.getCvFrame())
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+        counter+=1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1 :
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time   
+        print(f"FPS={fps:.2f}")
